e6f460fbd72452e0db885a8950d7c6a72618cdb3
NicholasBalsom <NicholasBalsom@gmail.com>
2026-03-01 20:41:01 -0330

    site config change

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from "@types";
 export const SITE_CONFIG: SiteConfig = {
   title: "Western Wave Robotics",
   authors: "Nicholas Balsom",
-  company: "Western Wave Robotics",
+  copyright: "Western Wave Robotics",
   description: "Competitive student team competing in the MATE ROV competition",
   siteLogo: "/westernwave_logo.png", // UPDATE
   // Header Navbar links
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,7 @@ export interface SiteConfig {
   siteLogo: string;
   navLinks: { text: string; href: string }[];
   authors: string;
-  company: string;
+  copyright: string;
   socialLinks: { text: string; href: string; logo: string; }[];
   socialImage: string;
   canonicalURL?: string;
